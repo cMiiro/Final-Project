a4c08e67a6ea5f51f1b393906d5d18f2fe08212a
Camlamenace <90792903+Camlamenace@users.noreply.github.com>
2023-04-06 11:07:09 +0200

    Add files via upload
    ajout de Mysql

--- enregistrement.php
+++ enregistrement.php
@@ -5,10 +5,25 @@
 </head>
 <body>
 <h1><?php 
-if(isset($_POST["nom"])){
-    setcookie("nom",$_POST["nom"],time()+3600);/* si un nom à été enregistrer alors on se souviendras du nom. De plus le cookies va rester pendant une heure et ensuite il faudras se reconnecter*/
-echo "ça marche";
-}echo $_COOKIE["nom"];
+if(isset ($_POST['nom']) && isset ($_POST['prenom']) && isset ($_POST['user']) && isset ($_POST['mdp'])){
+if($_POST['mdp']===$_POST['vmdp']){
+    $connexion = mysqli_connect ('localhost','root','','tsg_base') ;
+    $nom=$_POST['nom'];
+    $prenom=$_POST['prenom'];
+    $user=$_POST['user'];
+    $mdp=$_POST['mdp'];
+    $AddUser="INSERT INTO user VALUES('$nom' , '$prenom' ,'$user' ,'$mdp',NULL ,NULL,0 ,0);";
+    $resultat = mysqli_query ($connexion, $AddUser );
+    echo $resultat;
+    setcookie("user",$_POST["user"],time()+3600);/* si un nom à été enregistrer alors on se souviendras du nom. De plus le cookies va rester pendant une heure et ensuite il faudras se reconnecter*/
+    echo "Inscryption terminée";
+}else{
+    echo "pas le meme mots de passe";
+}
+}else{
+    echo"des informations ne sont pas rempli";
+}
+
 ?></h1>
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -8,10 +8,11 @@
     <tr><th>
     <form action="enregistrement.php" method="post">
 Nom :<input type="text" size="30" name="nom" value="Votre nom"><br>
-Mot de Passe :<input type="password" size="30" name="motdepasse"><br>
-Vérification Mot de Passe :<input type="password" size="30" name="vmotdepasse"><br>
-Date De Naissance :<input type="date" name="DateNaissance"><br>
-<input type="submit">
+Prénom :<input type="text" size="30" name="prenom" value="Votre Prénom"><br>
+Nom d'Utilisateur :<input type="text" size="30" name="user" value="Votre Nom D'utilisateur"><br>
+Mot de Passe :<input type="password" size="30" name="mdp"><br>
+Vérification Mot de Passe :<input type="password" size="30" name="vmdp"><br>
+<input type="submit" name="go">
 </form><br>
 </th></tr>
 </table>
