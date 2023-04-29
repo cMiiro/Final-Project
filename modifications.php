56bec84dd540100e49424c9f53129e2987d9f2b1
Camlamenace <90792903+Camlamenace@users.noreply.github.com>
2023-04-29 15:07:54 +0200

    Add files via upload

--- modifications.php
+++ modifications.php
@@ -15,7 +15,8 @@ require_once("fonction.php"); //rajouter sécurité une personne non connecté n
          Nouvelle Description <br> <textarea name="NewDescription" rows="10" cols="70" ></textarea><br>
          Ancien Mot De Passe : <input type="text" name="OldMdp"><br>
          Nouveau Mot De Passe :<input type="text" name="Newmdp"><br>
-        <input type="submit" name="go"></form>
+         <button type="submit" name="private">devenir compte privé</button><br>
+         <input type="submit" name="go"></form>
         <?php if(isset($_POST['Newprofil']) && isset($_POST['NewDescription'])){
             $user=$_SESSION["user"];
             if($_POST["Newprofil"]!==''){
@@ -35,6 +36,9 @@ require_once("fonction.php"); //rajouter sécurité une personne non connecté n
                     $req="UPDATE user SET mdp='$newmdp' WHERE NomUtil='$user' ";
                     $resultat = mysqli_query ($connexion, $req );
                 }
+            }
+            if(isset($_POST["private"])){
+                mysqli_query($connexion,"UPDATE user SET privé=1 WHERE NomUtil='$user'");
             }
             header ("Location:profil.php?user=$user");
         }
