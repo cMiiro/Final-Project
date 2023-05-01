03780d1d48fe5234d2b1f58b23fea799a8491925
Camlamenace <90792903+Camlamenace@users.noreply.github.com>
2023-05-01 14:55:00 +0200

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -8,17 +8,22 @@ require_once('header.php');
 <meta charset="ufr-8">
     </head>
 <body>
-    <a href="createPublication.php"> <img class="plus" src="image/plus.png"></a><br>
-    <datalist id="ListeUser">
-     <?php 
+<?php 
+     //on vérifie s'il y a des like ajouter ou suprimmer.
      like();
      unlike();
+?>
+
+    <a href="createPublication.php"> <img class="plus" src="image/plus.png"></a><br>
+    <datalist id="ListeUser">
+     <?php
      $listeUser=mysqli_query($connexion,"SELECT * From user ORDER BY abonées");
      while ($ligne=mysqli_fetch_assoc($listeUser)){
-    echo '<option value="'.$ligne['NomUtil'].'"></option>'; 
+    echo '<option value="'.$ligne['NomUtil'].'"></option>';  //on créer une option par utilisateur pour la barre de recherche. On les classe par ordre d'abonées.
     }
      ?>
 </datalist>
+<!-- Barre de recherche  -->
 <form action="accueil.php" method="post">
 <input type="search" name="recherche" list="ListeUser" placeholder="Valeur par defaut">
 <button type="submit">rechercher</button>
@@ -33,12 +38,12 @@ require_once('header.php');
         $NomUserPost=$ligne["NomUtil"];
         $req="Select * from $user"."abonnement WHERE user='$NomUserPost';";
         $estAbonne=mysqli_query ($connexion,$req);
-        $verification= mysqli_fetch_assoc ($estAbonne);
-        if($verification!==NULL){
+        $verification= mysqli_fetch_assoc ($estAbonne); //regarde si l'utilisateur est abonné à celui qui à poster le commentaire
+        if($verification!==NULL){//dans le cas ou il y a au moins un utilisateur du même nom que celui qui a posté le commentaire alors on affiche le commentaire(normalement il y en a au plus un).
             affichePublications($ligne);
         }}}
     
-        if(isset($_POST["recherche"])){
+        if(isset($_POST["recherche"])){//Dans le cas ou on a chercher avec la barre de recherche un utilisateur alors on va aller sur son profil.
             $userChercher= $_POST["recherche"];
             header ("Location:profil.php?user=$userChercher");
         }
