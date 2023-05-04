54a4a616057a84ae2dbb6e320a871e4f808467ca
Camlamenace <90792903+Camlamenace@users.noreply.github.com>
2023-05-04 11:15:14 +0200

    Add files via upload

---
+++ verificationSignalment.php
@@ -0,0 +1,22 @@
+<?php 
+require_once('header.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="profile.css">
+<meta charset="ufr-8">
+    </head>
+<body>
+<?php if(estModo($_SESSION["user"])===true){
+$signalement=mysqli_query($connexion,"Select * from signalement");
+while ($ligne=mysqli_fetch_assoc($signalement)){
+ $id=$ligne["idPublication"];
+echo "L'utilsateur ".$ligne["utilSignal"]."signal la publications suivantes <a href=supprimer.php?com=$id>";
+affichePublications2($id);
+echo"</a>";
+}
+
+} ?>
+</body> 
+</html>
